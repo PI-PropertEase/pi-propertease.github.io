a9e636aa5ca33b03ead58c0967748a488a41926f
Migas77 <miguel.belchior@ua.pt>
2024-02-28 12:19:18 +0000

    Updates

--- assets/js/5e95c892.048a1546.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkpropertease=self.webpackChunkpropertease||[]).push([[9647],{7121:(e,r,s)=>{s.r(r),s.d(r,{default:()=>l});s(6540);var a=s(4164),p=s(1003),t=s(7559),u=s(2831),c=s(781),n=s(4848);function l(e){return(0,n.jsx)(p.e3,{className:(0,a.A)(t.G.wrapper.docsPages),children:(0,n.jsx)(c.A,{children:(0,u.v)(e.route.routes)})})}}}]);
